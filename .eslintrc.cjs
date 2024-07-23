2a1c184f370e3130e03ad55cb46e67224adc66dc
abhishek270104 <abhishek.2024cs1184@kiet.edu>
2024-07-23 23:17:50 +0530

    eslint features updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,61 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true, 
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended', 
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended', 
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: '18.2',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  plugins: [
+    'react-refresh',
+    'jsx-a11y', 
+    'import', 
+    'prettier', 
+  ],
   rules: {
-    "react/prop-types": "off",
-    "no-unused-vars": "warn",
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'jsx-a11y/no-autofocus': 'warn', 
+    'jsx-a11y/anchor-is-valid': 'warn', 
+    'import/no-unresolved': 'error', 
+    'import/named': 'error', 
+    'import/default': 'error', 
+    'import/namespace': 'error', 
+    'prettier/prettier': 'warn', 
+    'no-console': 'warn', 
+    'no-debugger': 'warn', 
   },
-}
+};
